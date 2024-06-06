08a0d265e6fce7c0b969f2ab60a8e2bb2c7961dc
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-06-06 21:52:39 +0200

    Feat: Create the placeShip function

--- src/Models/GameBoard.mjs
+++ src/Models/GameBoard.mjs
@@ -2,17 +2,28 @@ const GameBoard = () => {
   const board = [];
   const rows = 10;
   const columns = 10;
-  const getBoard = () => {
+  const createBoard = () => {
     for(let i = 0; i < rows; i+=1){
       board.push([]);
       for(let j = 0; j < columns; j+=1){
         board[i].push({});
       }
     }
-    return board;
   };
-  
-  return {getBoard};
+  const placeShip = (coord, axis, ship ) => {
+    const [x, y] = [...coord];
+    if(axis === 'y'){
+      for(let i = 0; i < ship.health; i+=1){
+        board[i][y] = ship;
+      }
+    }else if(axis === 'x'){
+     
+      for(let i = 0; i < ship.health; i+=1){
+        board[x][i] = ship;
+      }
+    }
+  };
+  return {createBoard, placeShip, get board(){return board;}};
 };
 
 export default GameBoard;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-/* import Ship from './Models/Ship.mjs';
+import Ship from './Models/Ship.mjs';
+import GameBoard from './Models/GameBoard.mjs';
 
 const ship1 = Ship(3);
 
-console.log(ship1.hit());
-console.log(ship1.health);
-console.log(ship1.damage); */
 
-import GameBoard from './Models/GameBoard.mjs';
-
-const board1 = GameBoard().getBoard();
-
-console.log(board1);
+const board1 = GameBoard();
+board1.createBoard();
+board1.placeShip([0,0],'y', ship1);
+console.log(board1.board);
 

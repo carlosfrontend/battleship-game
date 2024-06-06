c42fe549c465601ac8959ea55eb6eec5dd0f520c
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-06-06 21:23:02 +0200

    Feat: Create a void board

--- src/Models/GameBoard.mjs
+++ src/Models/GameBoard.mjs
@@ -1,5 +1,18 @@
 const GameBoard = () => {
-    
+  const board = [];
+  const rows = 10;
+  const columns = 10;
+  const getBoard = () => {
+    for(let i = 0; i < rows; i+=1){
+      board.push([]);
+      for(let j = 0; j < columns; j+=1){
+        board[i].push({});
+      }
+    }
+    return board;
+  };
+  
+  return {getBoard};
 };
 
 export default GameBoard;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import Ship from './Models/Ship.mjs';
+/* import Ship from './Models/Ship.mjs';
 
 const ship1 = Ship(3);
 
 console.log(ship1.hit());
 console.log(ship1.health);
-console.log(ship1.damage);
+console.log(ship1.damage); */
+
+import GameBoard from './Models/GameBoard.mjs';
+
+const board1 = GameBoard().getBoard();
+
+console.log(board1);
 
